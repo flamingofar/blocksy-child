814f620f7344696b343539bd8a5b06bb8e59183d
malt0533 <malt0533@stud.kea.dk>
2021-11-05 17:26:45 +0100

    test

--- functions.php
+++ functions.php
@@ -5,6 +5,6 @@ if (! defined('WP_DEBUG')) {
 }
 
 add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
-function enqueue_parent_styles(){wp_enqueue_style('parent-style', get_template_directory_uri().'./style.css' );
+function enqueue_parent_styles(){wp_enqueue_style('parent-style', get_template_directory_uri().'/style.css', 99 );
 }
 ?>
