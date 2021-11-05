0bf065fd0662e8860d8f5ee6ba66476b03c46e22
malt0533 <malt0533@stud.kea.dk>
2021-11-05 17:21:07 +0100

    test

--- page-kurser.php
+++ page-kurser.php
@@ -17,7 +17,7 @@ get_header();
 
 <div id="primary" class="content-area">
 	<main id="main" class="site-main">
-		<div id="liste" style="background: red;"></div>
+		<div id="liste" ></div>
 
         <template>
                 <article>
