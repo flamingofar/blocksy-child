5f9a5a75aa29da0f21a1398f6b4c14d8fd6c84ea
malt0533 <malt0533@stud.kea.dk>
2021-11-11 13:32:30 +0100

    singleview first styling

--- single-kursus.php
+++ single-kursus.php
@@ -14,8 +14,9 @@
 
 get_header();
 ?>
+<main class="">
 <div id="primary" class="content-area">
-		<main id="main" class="site-main">
+		<main id="main " class="site-main singleview">
 		
 		<!-- First Section -->
 		<section class="single_section">
@@ -43,39 +44,67 @@ get_header();
 
 		<!-- Second Section -->
 		<section>
+			<div class="content_wrapper">
+				
+			
 			<div>
 				<h3></h3>
 				<p></p>
 			</div>
+			<div>
+				<img class="image" src="#" alt="" />
+			</div>
+				</div>
 		</section>
 		<!-- Third Section -->
 		<section>
+			<div class="content_wrapper">
 			<div>
 				<h3></h3>
 				<p></p>
 			</div>
+			<div>
+				<img class="image" src="#" alt="" />
+			</div>
+				</div>
 		</section>
 			<!-- Fourth Section -->
 		<section>
+			<div class="content_wrapper">
 			<div>
 				<h3></h3>
 				<p></p>
 			</div>
+			<div>
+				<img class="image" src="#" alt="" />
+			</div>
+				</div>
 		</section>
 			<!-- Fifth Section -->
 		<section>
+			<div class="content_wrapper">
 			<div>
 				<h3></h3>
 				<p></p>
 			</div>
+			<div>
+				<img class="image" src="#" alt="" />
+			</div>
+				</div>
 		</section>
 			<!-- Sixth Section -->
 		<section>
+			<div class="content_wrapper">
 			<div>
 				<h3></h3>
 				<p></p>
 			</div>
+			<div>
+				<img class="image" src="#" alt="" />
+			</div>
+				</div>
 		</section>
+			</main>
 
 		<script>
 			/** @format */
@@ -105,18 +134,23 @@ get_header();
 				// Singleview PODS
 				document.querySelector("section:nth-child(2) h3").textContent = kursus.titel_1;
 				document.querySelector("section:nth-child(2) p").textContent = kursus.tekst_1;
+				document.querySelector("section:nth-child(2) .image").src = "https://malteskjoldager.dk/kea/2.Semester/Tema_9/ungebyen/wp-content/uploads/2021/11/fn_verdensmaal.png"
 				
 				document.querySelector("section:nth-child(3) h3").textContent = kursus.titel_2;
 				document.querySelector("section:nth-child(3) p").textContent = kursus.tekst_2;
+				document.querySelector("section:nth-child(3) .image").src = "https://malteskjoldager.dk/kea/2.Semester/Tema_9/ungebyen/wp-content/uploads/2021/11/fn_verdensmaal.png"
 				
 				document.querySelector("section:nth-child(4) h3").textContent = kursus.titel_3;
 				document.querySelector("section:nth-child(4) p").textContent = kursus.tekst_3;
+				document.querySelector("section:nth-child(4) .image").src = "https://malteskjoldager.dk/kea/2.Semester/Tema_9/ungebyen/wp-content/uploads/2021/11/fn_verdensmaal.png"
 				
 				document.querySelector("section:nth-child(5) h3").textContent = kursus.titel_4;
 				document.querySelector("section:nth-child(5) p").textContent = kursus.tekst_4;
+				document.querySelector("section:nth-child(5) .image").src = "https://malteskjoldager.dk/kea/2.Semester/Tema_9/ungebyen/wp-content/uploads/2021/11/fn_verdensmaal.png"
 				
 				document.querySelector("section:nth-child(6) h3").textContent = kursus.titel_5;
 				document.querySelector("section:nth-child(6) p").textContent = kursus.tekst_5;
+				document.querySelector("section:nth-child(6) .image").src = "https://malteskjoldager.dk/kea/2.Semester/Tema_9/ungebyen/wp-content/uploads/2021/11/fn_verdensmaal.png"
 				
 			}
 			loadJSON()
